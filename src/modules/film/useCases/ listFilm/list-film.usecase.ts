5170a24af6d4acb46d07737f1c76790221f5cdf9
junioralcant <junioralcant111@gmail.com>
2022-10-30 11:32:58 -0300

    add params filter pagination in list film use case

--- src/modules/film/useCases/ listFilm/list-film.usecase.ts
+++ src/modules/film/useCases/ listFilm/list-film.usecase.ts
@@ -1,10 +1,14 @@
+import { IFilterListFilmDTO } from '../../dtos/filter-list-film.dots';
 import { IFilmRepository } from '../../repositories/ifilm.repository';
 
 export class ListFilmUseCase {
   constructor(private filmRepository: IFilmRepository) {}
 
-  async execute() {
-    const films = await this.filmRepository.list();
+  async execute({ skip, take }: IFilterListFilmDTO) {
+    const films = await this.filmRepository.list({
+      skip: skip || 0,
+      take: take || 10,
+    });
 
     return films;
   }
