9b639665deb0901736c89b1d7c49cce0d380a155
junioralcant <junioralcant111@gmail.com>
2022-10-30 14:37:25 -0300

    config swagger

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,13 +1,22 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
 import { filmRouter } from './routes/film.routes';
+import swaggerDocument from '../../../../swagger.json';
+import { redoclyRouter } from './routes/redocly.routes';
 
 const app = express();
 
 app.use(cors());
 
+app.use(
+  '/api-swagger',
+  swaggerUi.serve,
+  swaggerUi.setup(swaggerDocument)
+);
+
 app.use(express.json());
 
-app.use(filmRouter);
+app.use(filmRouter, redoclyRouter);
 
 app.listen(3333, () => console.log('Server in running on PORT 3333'));
