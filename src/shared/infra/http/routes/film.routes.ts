52d4a2679cacf8d2b425ce518b4ace8d19aa8093
junioralcant <junioralcant111@gmail.com>
2022-10-30 09:05:58 -0300

    create route list films

--- src/shared/infra/http/routes/film.routes.ts
+++ src/shared/infra/http/routes/film.routes.ts
@@ -1,8 +1,13 @@
 import { Router } from 'express';
+import { listFilmController } from '../../../../modules/film/useCases/ listFilm';
 import { createFilmController } from '../../../../modules/film/useCases/createFilm';
 import { searchAndRegisterFilmController } from '../../../../modules/film/useCases/searchRegisterFIlm';
 
 const filmRouter = Router();
+
+filmRouter.get('/films', (req, res) => {
+  listFilmController.handle(req, res);
+});
 
 filmRouter.post('/films', (req, res) => {
   createFilmController.handle(req, res);
